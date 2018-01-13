f667225dbe7215487b133d5827511a7bf84b8bd3
Adebayo Oluwadamilola <goshensoftinc@gmail.com>
2018-01-13 01:52:20 +0100

    moving to new app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,10 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
 
-
 @NgModule({
   declarations: [
     AppComponent,
-    HelloWorldComponent
+    HelloWorldComponent,
   ],
   imports: [
     BrowserModule
